df14ff1f6e18bdc39883e6d5dc7f7348463a4be8
Tof360 <chris_1232@hotmail.com>
2017-08-02 02:38:08 +0200

    Web page film v0.1 + liste de films home page

--- src/CT/CoreBundle/Controller/HomeController.php
+++ src/CT/CoreBundle/Controller/HomeController.php
@@ -19,4 +19,54 @@ class HomeController extends Controller
 
     }
 
+    public function loadMovieRepository($id)
+    {
+        $client = $this->get('tmdb.client');
+        $repository = new \Tmdb\Repository\MovieRepository($client);
+        $movie      = $repository->load($id, array('language' => 'fr', 'append_to_response' => 'credits'));
+
+        return $movie;
+
+    }
+
+    public function movieAction($id)
+    {
+        $movie = $this->loadMovieRepository($id);
+
+
+
+        /**
+         * $listPerson [] = $movie->getCredits()->getCrew();
+
+        $genres[] = array();
+        foreach ($movie->getGenres() as $genre) {
+        $genres [] = array('name' => $genre->getName());
+        }
+         *
+         **/
+
+        $genres = $movie->getGenres();
+        $crew = $movie->getCredits()->getCrew();
+        $cast = $movie->getCredits()->getCast();
+        $movie = array(
+            'title' => $movie->getTitle(),
+            'id' => $id,
+            'content' => $movie->getOverview(),
+            'date' => $movie->getReleaseDate(),
+            'backdropPath' => $movie->getBackdropPath(),
+            'posterPath' => $movie->getPosterPath(),
+            'originalTitle' => $movie->getOriginalTitle(),
+            'runTime' => $movie->getRuntime(),
+            'voteAverage' => $movie->getVoteAverage(),
+            'voteCount' => $movie->getVoteCount(),
+        );
+
+        return $this->render('CTCoreBundle:Movie:movie.html.twig', array(
+            'movie' => $movie,
+            'genres' => $genres,
+            'cast' => $cast,
+            'crew' => $crew,
+        ));
+    }
+
 }
